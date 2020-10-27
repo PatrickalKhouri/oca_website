574856f3c83f3323adc984cebb229b5023aa76a1
Patrick Khouri <patrick.darya@gmail.com>
2020-10-27 14:44:33 -0300

    Added all inputs to the form, altered the lables, and added validations of uniqueness and presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  validates :first_name, :last_name, :gender, :phone, :email, presence: true
+  validates :phone, :email, uniqueness: true
+  validates :gender, inclusion: { in: ["Masculino", "Feminino", "Outro"]}
 end
