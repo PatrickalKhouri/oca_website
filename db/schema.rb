e017994727fe9489ca2403709fcadbb1db7e2e9f
Patrick Khouri <patrick.darya@gmail.com>
2020-10-28 19:56:09 -0300

    created city seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_28_175737) do
+ActiveRecord::Schema.define(version: 2020_10_28_212130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 2020_10_28_175737) do
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["city_id"], name: "index_neighbourhoods_on_city_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+City.destroy_all
+Neighbourhood.destroy_all
+# Condominium.destroy_all
+# Apartment.destroy_all
+# User.destroy_all
+# Room.destroy_all
+# Furniture.destroy_all
+
+puts "Creating Cities..."
+
+rio_de_janeiro = City.create(name: "Rio de Janeiro")
+buzios = City.create(name: "Búzios")
+
+puts "Cities created"
+
+puts "Creating neighbourhoods.."
+
+copacabana = Neighbourhood.create(name: "Copacabana", city: rio_de_janeiro.id, description: "Copacabana é um bairro tradicional do Rio de Janeiro.É um bairro extremamente prático e central para toda a Zona Sul. Além de ser turístico, também é bastante comercial, onde é possível se encontrar lojas e serviços de qualquer espécie, com preços sempre em conta.")
+
+puts "Neighbourhoods created neighbourhoods.."
+
+
+puts "Seeds created"
