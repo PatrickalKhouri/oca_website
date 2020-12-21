4955789b80cf3466210a028be9ad8b24d4ee558b
Patrick Khouri <patrick.darya@gmail.com>
2020-12-21 15:32:38 -0300

    Did the first skeleton of the show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_11_194109) do
+ActiveRecord::Schema.define(version: 2020_12_21_172828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2020_12_11_194109) do
     t.bigint "condominium_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "parking_spot", default: 0
     t.index ["condominium_id"], name: "index_apartments_on_condominium_id"
   end
 
