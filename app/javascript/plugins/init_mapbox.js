2b049b95026bbba8c0111ffaf521cf76b8bce894
Patrick Khouri <patrick.darya@gmail.com>
2021-01-04 18:28:09 -0300

    Added the map, still need to change the marker

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,5 +1,12 @@
 import mapboxgl from 'mapbox-gl';
 
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  bounds.extend([ markers.lng, markers.lat ]);
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+};
+
+
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
@@ -9,7 +16,14 @@ const initMapbox = () => {
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
     });
-  }
+
+    const markers = JSON.parse(mapElement.dataset.markers);
+    new mapboxgl.Marker()
+        .setLngLat([ markers.lng, markers.lat ])
+        .addTo(map)
+
+        fitMapToMarkers(map, markers);
+  };
 };
 
 export { initMapbox };
