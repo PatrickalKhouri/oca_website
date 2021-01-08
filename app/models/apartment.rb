48aef00f67f595c87edbe5685519e4554b4bb167
Patrick Khouri <patrick.darya@gmail.com>
2021-01-08 17:06:49 -0300

    did the apartment validation tests

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -4,8 +4,8 @@ class Apartment < ApplicationRecord
   has_many :bookings
   validates :name, :number, :price, :m2, :room, :guest, :bed, :bathroom, :description, :the_space, :transportation, :has, :hasnt, :oca_id, :neighbourhood,  presence: true
   validates :oca_id, :name, uniqueness: true
-  validates :price, :room, :guest, :bed, :bathroom, :parking_spot => { greater_than_or_equal_to: 0}
-  validates :m2 => { greater_than: 0}
+  validates :price, :room, :guest, :bed, :bathroom, :parking_spot, :numericality => { greater_than_or_equal_to: 0}
+  validates :m2, :numericality => { greater_than: 0}
   validates :pet_friendly, inclusion: { in: [true, false]}
 
   def unavailable_dates
---
+++ spec/models/apartment_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe Apartment, type: :model do
+  context "validation tests" do
+    it 'ensures name presence' do
+      apartment = Apartment.new( number: "1006", price: 300, m2: 90, room: 3, guest: 6, bed: 5, bathroom: 2, pet_friendly: false, description: "test.", the_space: "test.", transportation: "test.",
+      has: "test", hasnt: "test", active: true , oca_id: "100", neighbourhood: "Copacabana").save
+      expect(apartment).to eq(false) 
+    end
+
+    it 'ensures number presence' do
+      apartment = Apartment.new( name: "ipa piraja", price: 300, m2: 90, room: 3, guest: 6, bed: 5, bathroom: 2, pet_friendly: false, description: "test.", the_space: "test.", transportation: "test.",
+      has: "test", hasnt: "test", active: true , oca_id: "100", neighbourhood: "Copacabana").save
+      expect(apartment).to eq(false) 
+    end
+
+    it 'ensures oca id uniqueness' do
+      apartment = Apartment.new( name: "ipa piraja", number: "100", price: 300, m2: 90, room: 3, guest: 6, bed: 5, bathroom: 2, pet_friendly: false, description: "test.", the_space: "test.", transportation: "test.",
+      has: "test", hasnt: "test", active: true , oca_id: "054", neighbourhood: "Copacabana").save
+      expect(apartment).to eq(false) 
+    end
+
+    it 'ensures price is greater than 0' do
+      apartment = Apartment.new( name: "ipa piraja", number: "100", price: -2, m2: 90, room: 3, guest: 6, bed: 5, bathroom: 2, pet_friendly: false, description: "test.", the_space: "test.", transportation: "test.",
+      has: "test", hasnt: "test", active: true , oca_id: "100", neighbourhood: "Copacabana").save
+      expect(apartment).to eq(false) 
+    end
+
+    it "ensures m2 can't be 0" do
+      apartment = Apartment.new( name: "ipa piraja", number: "100", price: 2, m2: 0, room: 3, guest: 6, bed: 5, bathroom: 2, pet_friendly: false, description: "test.", the_space: "test.", transportation: "test.",
+      has: "test", hasnt: "test", active: true , oca_id: "100", neighbourhood: "Copacabana").save
+      expect(apartment).to eq(false) 
+    end
+
+    it "ensures that pet friendly is true or false" do
+      apartment = Apartment.new( name: "ipa piraja", number: "100", price: 2, m2: 2, room: 3, guest: 6, bed: 5, bathroom: 2, pet_friendly: "false", description: "test.", the_space: "test.", transportation: "test.",
+      has: "test", hasnt: "test", active: true , oca_id: "100", neighbourhood: "Copacabana").save
+      expect(apartment).to eq(false) 
+    end
+  end
+end
