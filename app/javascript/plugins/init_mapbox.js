90e36eb211b368147588db563ad79beda4b1ad4a
Patrick Khouri <patrick.darya@gmail.com>
2021-01-15 16:42:30 -0300

    Trying to fix the map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -2,10 +2,9 @@ import mapboxgl from 'mapbox-gl';
 
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
-  bounds.extend([ markers.lng, markers.lat ]);
-  map.fitBounds(bounds, { padding: 90, maxZoom: 14, duration: 0 });
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
 };
-
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
@@ -18,12 +17,15 @@ const initMapbox = () => {
     });
 
     const markers = JSON.parse(mapElement.dataset.markers);
-    new mapboxgl.Marker()
-        .setLngLat([ markers.lng, markers.lat ])
-        /*.addTo(map)*/
-
-        fitMapToMarkers(map, markers);
-  };
+    markers.forEach((marker) => {
+      new mapboxgl.Marker()
+        .setLngLat([ marker.lng, marker.lat ])
+        .addTo(map);
+    });
+    
+    fitMapToMarkers(map, markers);
+  }
 };
+
 
 export { initMapbox };
